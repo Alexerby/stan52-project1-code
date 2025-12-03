226a06331dc9003ceda7195f1e81c11b59ba2851
alexerby <mail@alexandereriksson.se>
2025-12-03 12:59:19 +0100

    sequentially add models, remove tested ones

--- src/training/registry.py
+++ src/training/registry.py
@@ -12,25 +12,4 @@ MODEL_REGISTRY = {
         "scaler": True,
         "model": SVC(kernel="rbf", gamma="scale")
     },
-    "svm_poly": {
-        "scaler": True,
-        "model": SVC(kernel="poly", degree=3)
-    },
-    "random_forest": {
-        "scaler": False,
-        "model": RandomForestClassifier(n_estimators=200)
-    },
-    "gradient_boosting": {
-        "scaler": False,
-        "model": GradientBoostingClassifier()
-    },
-    "xgboost": {
-        "scaler": False,
-        "model": XGBClassifier(
-            max_depth=6,
-            n_estimators=200,
-            learning_rate=0.1,
-            subsample=0.8,
-        )
-    },
 }
